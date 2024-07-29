e7e72f71f85efa363540bc6173b3f61971c4c3f7
Maucaco <89039412+Maucaco@users.noreply.github.com>
2024-07-29 17:20:06 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,18 @@
+package aulinha;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		//VARIAVEIS
+		int numero = 3;
+		String frase= "ola mundo!";	
+		boolean var2 = true;
+		char var3 = 'c';
+		double var4 = 23.3;
+				
+		System.out.println(numero);
+		System.out.println(frase);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);	
+	}
+}
