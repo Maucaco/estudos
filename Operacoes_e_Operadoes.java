9cb97cc1141d694efdcbfb303ac707691d5ff445
Maucaco <89039412+Maucaco@users.noreply.github.com>
2024-07-30 14:45:27 -0300

    Add files via upload

---
+++ Operacoes_e_Operadoes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int vida1 = 100;
+    int vida2 = 150;
+    int vida3 = 50;
+    int vida_final = (vida1 + vida2 - vida3) /2;
+    System.out.println(vida_final);
+    
+    String nome = "Sarah";
+    int vida = 100;
+    if(vida != 90 && nome == "Sarah"){
+      System.out.println("Chamo Sarah e tenho 100 de vida!");
+     }
+   }
+ }
