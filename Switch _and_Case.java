5ca74e6964c2075e8e4837802ec52a58e8dc054e
Maucaco <89039412+Maucaco@users.noreply.github.com>
2024-07-30 14:28:31 -0300

    Add files via upload

---
+++ Switch _and_Case.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String[] args)
+   { 
+    int var1 = 90;
+    switch(var1)
+    {
+      case 90:
+        System.out.println("Minha vida e 90"); 
+        break;
+        default:
+          System.out.println("Minha vida e 100");
+          break;
+    }
+ }
+}
